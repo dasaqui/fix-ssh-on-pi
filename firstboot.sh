448e2883081df31b8e08849a257f718eb0089402
dasaqui <dasaqui@gmail.com>
2022-06-23 02:42:09 -0500

    easier host name

--- firstboot.sh
+++ firstboot.sh
@@ -4,6 +4,7 @@ macadd=$( ip -brief add | awk '/UP/ {print $1}' | sort | head -1 )
 if [ ! -z "${macadd}" ]
 then
   macadd=$( sed 's/://g' /sys/class/net/${macadd}/address )
+  macadd=rbpi-${macadd:8:12}
   sed "s/raspberrypi/${macadd}/g" -i /etc/hostname /etc/hosts
 fi
 /sbin/shutdown -r 5 "reboot in Five minutes"
