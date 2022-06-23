8a2aad1adbf63adbc59309a06f8acea5086ddf03
dasaqui <dasaqui@gmail.com>
2022-06-23 15:03:37 -0500

    correction in auto-login disable

--- firstboot.sh
+++ firstboot.sh
@@ -6,4 +6,9 @@ then
   macadd=$( sed 's/://g' /sys/class/net/${macadd}/address )
   sed "s/raspberrypi/${macadd}/g" -i /etc/hostname /etc/hosts
 fi
+
+# disable auto-login on tty1
+login_file="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+mv "${login_file}" "${login_file}.disabled"
+
 /sbin/shutdown -r 5 "reboot in Five minutes"
